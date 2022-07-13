fd8e4900d8fcd3aa35a0a785416e76b1a5b0c055
gelilaa <gelila.ali@outlook.com>
2022-07-13 17:03:39 +0200

    starting point settled

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import './style.css'
+export const Navbar=()=>{
+    return(
+       <div className='container-nav'> 
+       <div id="about_me__">
+		<span>about me </span><br/>
+	</div>
+	<div id="about_me__">
+		<span>my work</span><br/>
+		
+	</div>
+	<div id="about_me__">
+		<span>blogs</span><br/>
+		
+	</div>
+	</div>
+    )
+}
